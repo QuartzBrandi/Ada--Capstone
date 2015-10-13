94936b9679e319f10ae5ef7ec92a33c3e1f0d0d4
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-10-13 10:07:07 -0700

    Trying to figure out how to format app...

---
+++ public/javascripts/core.js
@@ -0,0 +1,4 @@
+// angular.module('visualMenu', ['todoController', 'todoService']);
+
+// module.exports = mongoose.model('restaurants', restaurantSchema);
+// module.exports = { 'test' : 'another test' }
