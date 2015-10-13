4129807670d4fc808c1fd001c278b08d40bf5b36
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-10-13 14:07:48 -0700

    PROGRESS! (Got search box to work for restaurant search.)

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -3,10 +3,17 @@
 
 	app.controller('restaurantController', ['$http', function($http) {
 		var restaurant = this;
-		restaurant.results = [];
-		$http.get('http://localhost:3000/api/search?name=teddy\'s%20burgers&location=honolulu')
-			.success(function(data) {
-				restaurant.results = data;
-			});
+		restaurant.search = "";
+
+		restaurant.searchRestaurants = function() {
+			restaurant.results = [];
+			var url = "http://localhost:3000/api/search?name=" + restaurant.search + "&location=honolulu";
+			$http.get(url)
+				.success(function(data) {
+					restaurant.results = data;
+				});
+			restaurant.search = "";
+		}
 	}]);
+
 })();
