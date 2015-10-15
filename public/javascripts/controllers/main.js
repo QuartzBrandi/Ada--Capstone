570d09a985491595011532a36b4bb19c552c3e5d
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-10-15 14:53:41 -0700

    Was working on trying to get images to show, but will have to take a different route.

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -3,17 +3,31 @@
 
 	app.controller('restaurantController', ['$http', function($http) {
 		var restaurant = this;
-		restaurant.search = "";
+		restaurant.searchName = "";
+		restaurant.searchLocation = "";
+
+		//TODO: Remove spaces if search has spaces???
 
 		restaurant.searchRestaurants = function() {
 			restaurant.results = [];
-			var url = "http://localhost:3000/api/search?name=" + restaurant.search + "&location=honolulu";
+			var url = "http://localhost:3000/api/search?name=" + restaurant.searchName + "&location=" + restaurant.searchLocation;
 			$http.get(url)
 				.success(function(data) {
 					restaurant.results = data;
 				});
-			restaurant.search = "";
+			restaurant.searchName = "";
+			restaurant.searchLocation = "";
+		}
+
+		restaurant.searchImages = function() {
+			restaurant.results = [];
+			var url = "http://localhost:3000/api/images?restaurant=" + restaurant.searchName + "&menuitem=kalua+pork";
+			$http.get(url)
+				.success(function(data) {
+					console.log("yo")
+				});
+			restaurant.searchName = "";
+			restaurant.searchLocation = "";
 		}
 	}]);
-
 })();
--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,11 @@ router.get('/api/search', function(req, res, next) {
   return api.apiController.restaurantSearch(req, res);
 });
 
+// search for restaurant (query "name" & "location")
+router.get('/api/images', function(req, res, next) {
+  return api.apiController.imageSearch(req, res);
+});
+
 // redirects if user tries to go anywhere else
 router.get('*', function(req, res, next) {
   res.redirect('/');
