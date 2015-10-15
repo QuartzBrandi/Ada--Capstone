4c5b8017118c5bc1e40a67997bce95c9cc56b0b8
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-10-15 13:52:58 -0700

    Still WIP, trying to get seed data to be dropped first.

--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -10,80 +10,65 @@
 
 
 var mongoose = require('mongoose');
-connect = mongoose.connect('mongodb://localhost/visualmenu');
-var assert = require('assert');
 
-// connect.connection.db.dropDatabase();
+mongoose.connect('mongodb://localhost/visualmenu');
+// mongoose.db.dropDatabase();
 
-var Restaurant = mongoose.model('Restaurant', {
-  name: String,
-  location: {
-    address: String,
-    last_updated: Date
-  },
-  menu: {
-    items: [{
-      name: String,
-      images: [{
-        origin: String,
-        url: String,
-        date_created: Date,
-        date_modified: Date
-      }],
-      date_created: Date,
-      date_modified: Date
-    }],
-    last_updated: Date
-  }
+// mongoose.connection.collections['visualmenu'].drop( function(err) {
+//     console.log('collection dropped');
+// });
+
+mongoose.connection.on('open', function(){
+  mongoose.connection.db.dropDatabase(function (err) {
+    console.log('db dropped');
+    // process.exit(0);
+    mongoose.connection.close();
+
+  });
 });
 
-var restaurants = []
-
-restaurants.push(new Restaurant({
-  name: "HELLO",
-  location: {
-    address: "555 HELLO ST. WA, Seattle"
-  }
-}));
-
-restaurants.push(new Restaurant({
-  name: "GOODBYE",
-  location: {
-    address: "666 GOODBYE ST. WA, Seattle"
-  }
-}));
-
-for (i = 0; i < restaurants.length; i++) {
-  restaurants[i].save(function (err) {
-    if (err) {
-      console.log("Restaurant", i, ":", err);
-    }
-  });
-}
-
-// var CategoryModel = mongoose.model('Category');
-
-// var data = [
-//   { 'name' : 'Barcelona' },
-//   { 'name' : 'Real Madrid' },
-//   { 'name' : 'Valencia' }
-// ];
-
-// Restaurant.collection.insert(data, function(err, r) {
+//
+// var Restaurant = mongoose.model('Restaurant', {
+//   name: String,
+//   location: {
+//     address: String,
+//     last_updated: Date
+//   },
+//   menu: {
+//     items: [{
+//       name: String,
+//       images: [{
+//         origin: String,
+//         url: String,
+//         date_created: Date,
+//         date_modified: Date
+//       }],
+//       date_created: Date,
+//       date_modified: Date
+//     }],
+//     last_updated: Date
+//   }
+// });
+//
+// var restaurants = []
+//
+// restaurants.push({
+//   name: "HELLO",
+//   location: {
+//     address: "555 HELLO ST. WA, Seattle"
+//   }
+// });
+//
+// restaurants.push({
+//   name: "GOODBYE",
+//   location: {
+//     address: "666 GOODBYE ST. WA, Seattle"
+//   }
+// });
+//
+// Restaurant.collection.insert(restaurants, function(err, r) {
 //   console.log(err);
 //   console.log(r);
 //
-//   mongoose.connection.close();
-// })
-
-// exports.seedCategories = function seedCategories() {
-//   Restaurant.find({}).exec(function (err, collection) {
-//       if (collection.length === 0) {
-//           Restaurant.create({ name: 'Hello' });
-//           Restaurant.create({ name: 'Goodbye' });
-//       }
-//   });
-// }
-
-
-mongoose.connection.close();
+  // mongoose.connection.close();
+// });
