8e560e8f76eb86a4f402d6e0823d134c2e7b2cc0
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-10-19 16:09:43 -0700

    See previous commit.

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -55,14 +55,19 @@
 	}]);
 
 	app.controller('GoogleCtrl', function() {
-  function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
-	}
-	console.log('WHYYY')
+	  function onSignIn(googleUser) {
+	    var profile = googleUser.getBasicProfile();
+	    console.log('ID: ' + profile.getId());
+	    console.log('Name: ' + profile.getName());
+	    console.log('Image URL: ' + profile.getImageUrl());
+	    console.log('Email: ' + profile.getEmail());
+
+			var id_token = googleUser.getAuthResponse().id_token;
+			console.log("ID Token: " + id_token);
+		}
+		console.log('WHYYY')
+
+
   	window.onSignIn = onSignIn;
 	});
 
