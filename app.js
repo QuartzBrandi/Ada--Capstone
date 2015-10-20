cf26a5b3bfba9955ece56818d4ebac321325b2ed
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-10-20 15:12:41 -0700

    Moved route to routes folder.

--- app.js
+++ app.js
@@ -28,53 +28,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static('public/images/uploads'));
-
-// var upload = multer({ dest: './public/images/uploads/'}).single('photoupload')
-
-var mime = require('mime');
-
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/images/uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + '.' + mime.extension(file.mimetype))
-  }
-});
-
-var upload = multer({ storage: storage }).single('photoupload');
-
-app.post('/api/photo', function (req, res) {
-  console.log("THIS IS IT", req);
-  upload(req, res, function (err) {
-    if (err) {
-      // An error occurred when uploading
-      console.log("ERROR", err)
-      console.log("req", req)
-      return
-    } else {
-
-    console.log("req", req)
-    console.log("res", res)
-    console.log("NO ERROR")
-    return res.json({ a: 1 });
-    // Everything went fine
-    }
-  })
-})
-
-// app.use(multer({ dest: './public/images/uploads/',
-//     rename: function (fieldname, filename) {
-//         return filename+Date.now();
-//     },
-//     onFileUploadStart: function (file) {
-//         console.log(file.originalname + ' is starting ...');
-//     },
-//     onFileUploadComplete: function (file) {
-//         console.log(file.fieldname + ' uploaded to  ' + file.path)
-//     }
-// }).single('photo'));
 
 app.use('/', routes);
 app.use('/api/restaurants', restaurants);
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,39 @@ var root_dir = require('../root_dir.js');
 
 var multer = require('multer');
 
+var mime = require('mime');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + '.' + mime.extension(file.mimetype))
+  }
+});
+
+var upload = multer({ storage: storage }).single('photoupload');
+
+router.post('/api/photo', function (req, res) {
+  console.log("THIS IS IT", req);
+  upload(req, res, function (err) {
+    if (err) {
+      // An error occurred when uploading
+      console.log("ERROR", err)
+      console.log("req", req)
+      return
+    } else {
+
+    console.log("req", req)
+    console.log("res", res)
+    console.log("NO ERROR")
+    return res.json({ a: 1 });
+    // Everything went fine
+    }
+  })
+})
+
+
 // var upload = multer({ dest: '../public/images/uploads/'}).single('photoupload')
 //
 // router.post('/api/photo', function (req, res) {
