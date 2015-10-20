3bb736c853e3fcb12fe0171f8e6bfe3887e28eda
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-10-20 15:05:43 -0700

    OMG got file uploads to work... (The longer this project goes on, the more uglier the code becomes.)

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var routes = require('./routes/index');
 var restaurants = require('./routes/restaurants');
 var users = require('./routes/users');
 
+var multer = require('multer');
+
 var app = express();
 
 // view engine setup
@@ -26,10 +28,58 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public/images/uploads'));
+
+// var upload = multer({ dest: './public/images/uploads/'}).single('photoupload')
+
+var mime = require('mime');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + '.' + mime.extension(file.mimetype))
+  }
+});
+
+var upload = multer({ storage: storage }).single('photoupload');
+
+app.post('/api/photo', function (req, res) {
+  console.log("THIS IS IT", req);
+  upload(req, res, function (err) {
+    if (err) {
+      // An error occurred when uploading
+      console.log("ERROR", err)
+      console.log("req", req)
+      return
+    } else {
+
+    console.log("req", req)
+    console.log("res", res)
+    console.log("NO ERROR")
+    return res.json({ a: 1 });
+    // Everything went fine
+    }
+  })
+})
+
+// app.use(multer({ dest: './public/images/uploads/',
+//     rename: function (fieldname, filename) {
+//         return filename+Date.now();
+//     },
+//     onFileUploadStart: function (file) {
+//         console.log(file.originalname + ' is starting ...');
+//     },
+//     onFileUploadComplete: function (file) {
+//         console.log(file.fieldname + ' uploaded to  ' + file.path)
+//     }
+// }).single('photo'));
 
 app.use('/', routes);
 app.use('/api/restaurants', restaurants);
 app.use('/api/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,67 @@ var router = express.Router();
   // (Get a string of the project root directory.)
 var root_dir = require('../root_dir.js');
 
+var multer = require('multer');
+
+// var upload = multer({ dest: '../public/images/uploads/'}).single('photoupload')
+//
+// router.post('/api/photo', function (req, res) {
+//   console.log("THIS IS IT", req);
+//   upload(req, res, function (err) {
+//     if (err) {
+//       // An error occurred when uploading
+//       console.log("ERROR", err)
+//       console.log("req", req)
+//       return
+//     } else {
+//
+//     console.log("req", req)
+//     console.log("res", res)
+//     console.log("NO ERROR")
+//     return res.json({ a: 1 });
+//     // Everything went fine
+//     }
+//   })
+// })
+
+
+// var upload = multer({ dest: '../public/images/uploads/'});
+//
+// router.post('/api/photo', upload.single('photo'), function(req,res,next) {
+// // router.post('/api/photo',function(req,res,next){
+// console.log("req", req.file)
+// console.log("req", req.body)
+//
+//
+//     // upload.(req,res,function(err) {
+//     //     if(err) {
+//     //         return res.end("Error uploading file.");
+//     //     }
+//     //     res.end("File is uploaded");
+//     // });
+// });
+
+// var upload = multer({ dest: './uploads/'}).single('photoupload')
+//
+// router.post('/api/photo', function (req, res) {
+//   console.log("THIS IS IT", req);
+//   upload(req, res, function (err) {
+//     if (err) {
+//       // An error occurred when uploading
+//       console.log("ERROR", err)
+//       console.log("req", req)
+//       return
+//     } else {
+//
+//     console.log("req", req)
+//     console.log("res", res)
+//     console.log("NO ERROR")
+//     return res.json({ a: 1 });
+//     // Everything went fine
+//     }
+//   })
+// })
+
 // single page app
 // (self note: angular will change things on the front-end)
 router.get('/', function(req, res, next) {
