b62e64fd5c7a576e5ddd1ba34332d21fc27ec03d
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-10-22 09:52:09 -0700

    Well that was relatively painless.

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -3,25 +3,25 @@
 	// var app = angular.module('visualMenuControllers', [ ])
 	var app = angular.module('visualMenu', ['ngRoute'])
 
-	scotchApp.config(function($routeProvider) {
+	app.config(function($routeProvider) {
 			$routeProvider
 
 					// route for the home page
 					.when('/', {
 							templateUrl : 'pages/home.html',
-							controller  : 'mainController'
+							// controller  : 'mainController'
 					})
 
 					// route for the about page
 					.when('/about', {
 							templateUrl : 'pages/about.html',
-							controller  : 'aboutController'
+							// controller  : 'aboutController'
 					})
 
 					// route for the contact page
 					.when('/contact', {
 							templateUrl : 'pages/contact.html',
-							controller  : 'contactController'
+							// controller  : 'contactController'
 					});
 	});
 
