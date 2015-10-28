59a12c66bd7b3c1ee3b8992d67229b096c5bfc45
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-10-28 16:03:03 -0700

    This is a dumb npm if this works.

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -7,9 +7,9 @@
 	var app = angular.module('visualMenuControllers', ['ngRoute', 'ngAnimate', 'ngTouch', 'ui.bootstrap', 'ngFileUpload'])
 
 	// var uriSite = process.env.NODE_ENV == "production" ? "http://ada-capstone-production.elasticbeanstalk.com/" : "http://localhost:3000/"
-	var uriSite = "http://ada-capstone-production.elasticbeanstalk.com/";
+	// var uriSite = "http://ada-capstone-production.elasticbeanstalk.com/";
 	// var uriSite = "http://www.picto-menu.com/";
-	// var uriSite = "http://localhost:3000/";
+	var uriSite = "http://localhost:3000/";
 
 	app.controller('searchController', ['$scope', '$http', function($scope, $http) {
 		$scope.searchName = "";
